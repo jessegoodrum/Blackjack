91c13587fbaca796f13dee0ea470a21f3fc066da
jessegoodrum <103291307+jessegoodrum@users.noreply.github.com>
2024-08-18 10:16:54 -0700

    Game is playable

---
+++ index.js
@@ -0,0 +1,85 @@
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+let player = {
+    name: "Jesse",
+    chips: "200"
+}
+
+playerEl.textContent = player.name + " Chips: "+ player.chips
+
+function bet(number){
+    if(number === 1){
+        player.chips - number
+    }else if(number === 5){
+        player.chips - number
+    }else{
+        player.chips - number
+    }
+    //player can click 3 buttons that have 1 5 10, that number gets deducted from chips
+    //if player doesnt have enough chips they can't bet
+}
+
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random()*13)+1
+    if(randomNumber > 10){
+        return 10
+    }else if(randomNumber === 1){
+        return 11
+    }else{
+        return randomNumber
+    }
+}
+
+
+function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard,secondCard]
+    sum = firstCard + secondCard
+    renderGame() 
+}
+
+function renderGame(){
+    cardsEl.textContent = "Cards: "
+    for(let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: "+ sum
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackjack = true
+        isAlive = false
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+        
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard(){
+
+    if(isAlive && !hasBlackJack){
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+
+    
+    
+}
